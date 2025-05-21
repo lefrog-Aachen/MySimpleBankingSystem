0bd133edd7f2132106d905a162b69b3614d28794
lefrog06 <github.passably243@passmail.net>
2025-05-21 14:52:00 +0200

    updates

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -3,7 +3,7 @@ import random
 
 class Accounts:
     all_accounts = {}
-    IIN = 400000
+    IIN = '400000'
 
     def __init__(self):
         self.card_number = ''
@@ -13,21 +13,49 @@ class Accounts:
         self.balance = 0
         Accounts.all_accounts[customer] = self
 
-    def create_account(self) -> None:
+    def create_account(self) -> str:
         customer_id: int = random.randrange(000000000, 999999999)
         while customer_id in Accounts.all_accounts:
             customer_id: int = random.randrange(000000000, 999999999)
         self.card_number = f'{Accounts.IIN}{customer_id:09d}'
+        return str(customer_id)
 
     def create_pin(self):
         pin_id = random.randrange(0000,9999)
         self.pin = f'{pin_id:04d}'
 
-def check_login():
-    pass
+def check_login(card, pin):
+    res = False
+    if card[0:6] == Accounts.IIN and card[6:] in Accounts.all_accounts:
+        if pin == Accounts.all_accounts[card[6:]].pin:
+            print('You have succesfully logged in!')
+            res = True
+    return res
+
+def account_operations(account_id):
+    while True:
+        operation = int(input('''
+        1. Balance
+        2. Log out
+        0. Exit
+        '''))
+        if operation == 0:
+            # print('Bye!')
+            break
+        elif operation == 1:
+            print(f'Balance: {Accounts.all_accounts[account_id].balance}')
+        elif operation == 2:
+            break
+
 
 def account_login():
-    pass
+    # login into account
+    card_id = input('Enter your card number:')
+    pin_id = input('Enter your PIN:')
+    if check_login(card_id, pin_id):
+        account_operations(card_id[6:])
+    else:
+        print('Wrong card number or PIN!')
 
 def create_card():
     """
@@ -43,25 +71,18 @@ def create_card():
     Your card PIN:
     {new_account.pin}
     """)
+# Main system prompt
 
-choice = int(input("""
-1. Create an account
-2. Log into account
-0. Exit
-"""))
-
-if choice == 1:
-    create_card()
-elif choice == 2:
-    account_login()
-else:
-    print("Bye!")
-
-# toto = Accounts()
-# tutu = Accounts()
-print(Accounts.all_accounts)
-for account in Accounts.all_accounts:
-    print(account)
-    print()
-    for x in Accounts.all_accounts:
-        print(Accounts.all_accounts[x].pin)
+while True:
+    choice = int(input('''
+    1. Create an account
+    2. Log into account
+    0. Exit
+    '''))
+    if choice == 0:
+        print('Bye!')
+        break
+    elif choice == 1:
+        create_card()
+    elif choice == 2:
+        account_login()
