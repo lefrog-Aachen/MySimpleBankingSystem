ef0d4bea7bc240695c5e95f94325da43ed84445a
lefrog06 <github.passably243@passmail.net>
2025-05-21 15:58:18 +0200

    updates

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -1,6 +1,5 @@
 import random
 
-
 class Accounts:
     all_accounts = {}
     IIN = '400000'
@@ -14,38 +13,46 @@ class Accounts:
         Accounts.all_accounts[customer] = self
 
     def create_account(self) -> str:
-        customer_id: int = random.randrange(000000000, 999999999)
+        checksum = 5
+        customer_id = gen_customer_id()+str(checksum)
         while customer_id in Accounts.all_accounts:
-            customer_id: int = random.randrange(000000000, 999999999)
-        self.card_number = f'{Accounts.IIN}{customer_id:09d}'
+            checksum = 9
+            customer_id = gen_customer_id()+str(checksum)
+        self.card_number = f'{Accounts.IIN}{customer_id}'
         return str(customer_id)
 
     def create_pin(self):
         pin_id = random.randrange(0000,9999)
         self.pin = f'{pin_id:04d}'
+def gen_customer_id():
+    return f'{random.randrange(000000000, 999999999):09d}'
 
 def check_login(card, pin):
     res = False
+    # print(f'Checking {card} and {pin}')
+    # print(f'{card[0:6]} vs. IIN {Accounts.IIN}')
+    # print(f'{card[6:]} is in Accounts: {card[6:] in Accounts.all_accounts}')
+    # print(f'{Accounts.all_accounts.keys()}')
     if card[0:6] == Accounts.IIN and card[6:] in Accounts.all_accounts:
         if pin == Accounts.all_accounts[card[6:]].pin:
-            print('You have succesfully logged in!')
+            print('You have successfully logged in!')
             res = True
     return res
 
 def account_operations(account_id):
-    while True:
-        operation = int(input('''
-        1. Balance
-        2. Log out
-        0. Exit
-        '''))
-        if operation == 0:
-            # print('Bye!')
-            break
-        elif operation == 1:
-            print(f'Balance: {Accounts.all_accounts[account_id].balance}')
-        elif operation == 2:
-            break
+    # while True:
+    operation = int(input('''
+    1. Balance
+    2. Log out
+    0. Exit
+    '''))
+    if operation == 0:
+        print('Bye!')
+        # break
+    elif operation == 1:
+        print(f'Balance: {Accounts.all_accounts[account_id].balance}')
+    # elif operation == 2:
+    #     break
 
 
 def account_login():
@@ -64,14 +71,16 @@ def create_card():
     prints card # and pin
     """
     new_account = Accounts()
-    print(f"""
-    Your card has been created
-    Yout card number:
-    {new_account.card_number}
-    Your card PIN:
-    {new_account.pin}
-    """)
+    print('Your card has been created')
+    print('Your card number:')
+    print(new_account.card_number)
+    print('Your card PIN: ')
+    print(new_account.pin)
 # Main system prompt
+
+
+
+
 
 while True:
     choice = int(input('''
