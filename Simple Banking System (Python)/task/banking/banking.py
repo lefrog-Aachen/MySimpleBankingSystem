cfecdb287a743883b391ed80cad72205118b5918
lefrog06 <github.passably243@passmail.net>
2025-05-21 14:14:38 +0200

    updates

--- Simple Banking System (Python)/task/banking/banking.py
+++ Simple Banking System (Python)/task/banking/banking.py
@@ -2,22 +2,22 @@ import random
 
 
 class Accounts:
-    all_accounts = []
+    all_accounts = {}
     IIN = 400000
 
     def __init__(self):
-        self.customer = ''
+        self.card_number = ''
         self.pin = ''
-        self.create_account()
+        customer = self.create_account()
         self.create_pin()
         self.balance = 0
-        Accounts.all_accounts.append(self)
+        Accounts.all_accounts[customer] = self
 
     def create_account(self) -> None:
         customer_id: int = random.randrange(000000000, 999999999)
         while customer_id in Accounts.all_accounts:
             customer_id: int = random.randrange(000000000, 999999999)
-        self.customer = f'{customer_id:09d}'
+        self.card_number = f'{Accounts.IIN}{customer_id:09d}'
 
     def create_pin(self):
         pin_id = random.randrange(0000,9999)
@@ -39,7 +39,7 @@ def create_card():
     print(f"""
     Your card has been created
     Yout card number:
-    {Accounts.IIN}{new_account.customer}
+    {new_account.card_number}
     Your card PIN:
     {new_account.pin}
     """)
@@ -62,9 +62,6 @@ else:
 print(Accounts.all_accounts)
 for account in Accounts.all_accounts:
     print(account)
-    print(Accounts.all_accounts[0])
+    print()
     for x in Accounts.all_accounts:
-        print(x.pin)
-
-# initial test
-# Can't get this foing woth GIT
+        print(Accounts.all_accounts[x].pin)
